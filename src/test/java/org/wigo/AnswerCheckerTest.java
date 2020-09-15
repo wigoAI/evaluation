097994be252debee71cf83cbe2310a5c6d729952
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2020-09-15 16:18:54 +0900

    sentence splitter answer checker

---
+++ src/test/java/org/wigo/AnswerCheckerTest.java
@@ -0,0 +1,50 @@
+package org.wigo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnswerCheckerTest {
+
+
+    @Test
+    public void setAnswerCheckerTest() {
+        AnswerChecker answerChecker = AnswerChecker.setAnswerCheckerByAnswerFile("/data/answer1.txt");
+        List<String> answerList = answerChecker.getAnswerList();
+
+        for (String str : answerList) {
+            System.out.println("[" + str + "]");
+        }
+
+    }
+
+    @Test
+    public void checkAllCorrect() {
+        List<String> answer = new ArrayList<>();
+        List<String> sheet = new ArrayList<>();
+        answer.add("안녕하세요");
+        answer.add("반갑습니다.");
+        answer.add("전 조승현 입니다.");
+
+        sheet.add("안녕하세요");
+        sheet.add("반갑습니다.");
+        sheet.add("전 조승현 입니다.");
+
+
+
+        AnswerChecker answerChecker = AnswerChecker.setAnswerCheckerByAnswerList(answer);
+        System.out.println(answerChecker.checkAnswer(sheet));
+
+    }
+
+    @Test
+    public void getExampleFileTest() {
+        List<String> exampleFiles = AnswerChecker.getExampleFiles();
+
+        for(String str : exampleFiles) {
+            System.out.println(str);
+        }
+    }
+}
